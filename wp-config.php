2113bb7174653660395d28712d832fb8773b75ba
ngthongha <ngthongha92@gmail.com>
2024-09-07 09:17:48 +0700

    commit create code base

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'kientrucnamhai');
+define('DB_NAME', 'bim4c');
 
 /** MySQL database username */
 define('DB_USER', 'root');
