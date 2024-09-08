65993e69db929d85747b826d01972d41034ee297
ngthongha <ngthongha92@gmail.com>
2024-09-08 23:43:23 +0700

    commit create code base

--- wp-content/themes/flatsome/footer.php
+++ wp-content/themes/flatsome/footer.php
@@ -19,10 +19,5 @@ global $flatsome_opt;
 </div><!-- #wrapper -->
 
 <?php wp_footer(); ?>
-<div class="box61">
-<button class="nut11"><a style="color:#fff" href="https://zalo.me/0868310666">Chat zalo</a></button>
-<button class="nut21"><a style="color:#fff" href="https://www.messenger.com/t/XaydungNamHai">Chat Facebook</a></button>
-<button class="nut31"><a style="color:#fff" href="tel: 0868310666">Hotline: 0868 310 666</a></button>
-</div>
 </body>
 </html>
